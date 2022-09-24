bc6a58354ed52b384a3d2cde9d3dd19a3df793f0
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-09-24 14:54:38 -0500

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,31 @@
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Actions"
+git fetch origin gh-pages
+
+if [ ! -d "dist" ]; then
+  mkdir dist
+fi;
+
+cp -Rfv build/* ./dist/
+
+git checkout gh-pages
+
+for dir in ./*
+do
+  if [ "$dir" == "./dist" ]; then
+    continue
+  fi
+
+  rm -rf "$dir"
+done
+
+cp -Rfv dist/* ./
+rm -rf dist
+
+echo "mitchlaw00.com" > CNAME
+
+git add .
+git branch -D gh-pages
+git branch -m gh-pages
+git commit -m "Update JavaDocs ($1)"
+git push -f origin gh-pages
